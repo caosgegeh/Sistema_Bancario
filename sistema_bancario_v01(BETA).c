81fb49558d23ab57ff918594921217a3252ec62f
Gelvani <calistenia.brasil.experto@gmail.com>
2026-07-08 16:25:22 -0300

    Update sistema_bancario_v01(BETA).c

--- sistema_bancario_v01(BETA).c
+++ sistema_bancario_v01(BETA).c
@@ -86,7 +86,7 @@ int main() {
     //  inicializando as ferramentas
     struct Ferramentas f = {
         .saldo = 0,
-        .juros_saldo = 0.01, // Juros sobre o saldo de 0,8% a.m.
+        .juros_saldo = 0.01, // Juros sobre o saldo de 1% a.m.
         .ID = -1
     };
 
